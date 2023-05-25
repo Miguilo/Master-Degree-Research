b6c6b23a96479174d8ab2329f0864dca65aba152
Miguel Senra <mfasenra@hotmail.com>
2023-05-25 14:39:05 -0300

    test

---
+++ Positron-Binding-Energy/src/evaluate_models.py
@@ -0,0 +1,66 @@
+"""
+This file contain functions to evaluate models or metrics to be used.
+"""
+import numpy as np
+from sklearn.model_selection import cross_validate
+
+
+def display_scores(scores):
+    """
+    scores : Teste Scores of the cross_val procedure
+    """
+
+    print("Test Scores:", scores)
+    print(f"Mean Test Scores: {np.round(scores.mean(), 2)}")
+    print(f"Std of Scores: {np.round((scores.std()),2)}")
+    score_min = np.round(scores.min(), 2)
+    score_max = np.round(scores.max(), 2)
+    print(f"Min and Max of Scores: {score_min}, {score_max}")
+
+
+def get_cross_validation_scores(estimator, X, y, cv):
+    """
+    Algorithm to get the cv scores (train and test)
+    """
+    scoring = [
+        "neg_root_mean_squared_error",
+        "neg_mean_absolute_percentage_error",
+    ]  # Métricas de avaliação desejadas
+    cv_results = cross_validate(
+        estimator, X, y, cv=cv, scoring=scoring, return_train_score=True
+    )
+
+    train_scores = {
+        "neg_root_mean_squared_error": -cv_results[
+            "train_neg_root_mean_squared_error"
+        ],
+        "neg_mean_absolute_percentage_error": -cv_results[
+            "train_neg_mean_absolute_percentage_error"
+        ],
+    }
+
+    test_scores = {
+        "neg_root_mean_squared_error": -cv_results[
+            "test_neg_root_mean_squared_error"
+        ],
+        "neg_mean_absolute_percentage_error": -cv_results[
+            "test_neg_mean_absolute_percentage_error"
+        ],
+    }
+
+    return train_scores, test_scores
+
+
+def show_metrics(
+    estimator, x, y, cv, scoring="neg_mean_absolute_percentage_error"
+):
+    """Algoritmo que devolve a análise da métrica a ser analisada em problema
+    de regressão pra um data set já finalizado.
+
+    scoring : The score that you want to show the metrics."""
+    # Parte do CV
+
+    train_score, test_score = get_cross_validation_scores(estimator, x, y, cv)
+
+    display_scores(test_score[scoring])
+    print(f"\nTrain Score: {np.round(np.mean(train_score[scoring]), 2)}\n")
--- Positron-Binding-Energy/src/modifying_data.py
+++ Positron-Binding-Energy/src/modifying_data.py
@@ -2,26 +2,54 @@
 Module to process raw data into data's that I want to utilize.
 """
 
+import os
+import warnings
+
 import datasist as ds
+import hydra
 import numpy as np
 import pandas as pd
-import warnings
-import hydra
-import os
 from omegaconf import DictConfig
+from sklearn.preprocessing import FunctionTransformer
 
 warnings.filterwarnings("ignore")
+
+
+def log_transform(x):
+    """
+    Function to log transform some np.array
+    """
+    return np.log(x)
+
+
+def log_transform_dataframe(dataframe, column_name):
+    """
+    dataframe = DataFrame to have the columns transformed
+    column_name = List containing the name of the columns
+    """
+    dataframe = dataframe.copy()
+
+    transformer = FunctionTransformer(log_transform)
+    dataframe_transformed = transformer.fit_transform(dataframe[column_name])
+
+    dataframe[column_name] = dataframe_transformed
+
+    return dataframe
+
 
 def get_absolute_path(file_path):
     """
     Takes the absolute path even with the change of
     directory due to hydra.
     """
-    abs_path = os.path.abspath(os.path.join(hydra.utils.get_original_cwd(), 
-                                            file_path))
+    abs_path = os.path.abspath(
+        os.path.join(hydra.utils.get_original_cwd(), file_path)
+    )
     return abs_path
 
+
 def make_processed_data(raw_path, processed_path, columns_to_drop):
+    "Function to process my apolar/polar data"
     abs_file_path = get_absolute_path(raw_path)
     print(abs_file_path)
 
@@ -37,15 +65,19 @@ def make_processed_data(raw_path, processed_path, columns_to_drop):
             inplace=True,
         )
         df = ds.feature_engineering.drop_missing(df, percent=10)
-    
+
     else:
         raise TypeError("Only boolean are allowed")
-    
+
     new_path = get_absolute_path(processed_path)
     df.to_csv(new_path, index=False)
 
-def make_final_data(raw_path, final_path, columns_to_drop):
 
+def make_final_data(raw_path, final_path, columns_to_drop):
+    """
+    Function to make the final data to be used in apolar/polar
+    molecules
+    """
     abs_file_path = get_absolute_path(raw_path)
 
     df = pd.read_csv(abs_file_path)
@@ -70,30 +102,54 @@ def make_final_data(raw_path, final_path, columns_to_drop):
     new_path = get_absolute_path(final_path)
     df.to_csv(new_path, index=False)
 
+
 @hydra.main(config_path="../config", config_name="main.yaml")
 def make_apolar_data(config: DictConfig) -> pd.DataFrame:
+    """
+    Making processed and final data for all apolar molecules
+    """
+    make_processed_data(
+        config.apolar.raw,
+        config.apolar.processed.path,
+        config.process.to_drop.apolar,
+    )
 
-    make_processed_data(config.apolar.raw, config.apolar.processed.path,
-                        config.process.to_drop.apolar)
-   
 
 @hydra.main(config_path="../config", config_name="main.yaml")
 def make_aniso_apolar_data(config: DictConfig):
-    
-    make_final_data(config.apolar.raw, config.apolar.final.path,
-                        config.process.to_drop.apolar)
-    
+    """Making Processed and final data
+    for partial molecules"""
+    make_final_data(
+        config.apolar.raw,
+        config.apolar.final.path,
+        config.process.to_drop.apolar,
+    )
+
+
 @hydra.main(config_path="../config", config_name="main.yaml")
 def make_polar_data(config: DictConfig) -> pd.DataFrame:
+    """
+    Making Processed and final data for all
+    polar molecules
+    """
+    make_processed_data(
+        config.polar.raw,
+        config.polar.processed.path,
+        config.process.to_drop.polar,
+    )
 
-    make_processed_data(config.polar.raw, config.polar.processed.path,
-                        config.process.to_drop.polar)
 
 @hydra.main(config_path="../config", config_name="main.yaml")
 def make_aniso_polar_data(config: DictConfig):
-    
-    make_final_data(config.polar.raw, config.polar.final.path,
-                        config.process.to_drop.polar)
+    """
+    Making Processed and final data
+    for partial polar molecules
+    """
+    make_final_data(
+        config.polar.raw, config.polar.final.path, config.process.to_drop.polar
+    )
+
+
 if __name__ == "__main__":
     make_apolar_data()
     make_aniso_apolar_data()
