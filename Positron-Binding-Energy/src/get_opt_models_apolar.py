5355ca54f9d54356ec8cae830b41c4ec800e4fb9
Miguel Senra <mfasenra@hotmail.com>
2023-06-20 12:30:41 -0300

    updating config files

--- Positron-Binding-Energy/src/get_opt_models_apolar.py
+++ Positron-Binding-Energy/src/get_opt_models_apolar.py
@@ -7,10 +7,10 @@ import hydra
 import pandas as pd
 from modifying_data import get_absolute_path
 from omegaconf import DictConfig
+from skopt.space.space import Real
 
 
 def get_data(path: str):
-
     abs_path = get_absolute_path(path)
     df = pd.read_csv(abs_path)
     return df
@@ -19,9 +19,18 @@ def get_data(path: str):
 @hydra.main(config_path="../config", config_name="main.yaml")
 def main(cfg: DictConfig):
     # df_all_apolar = get_data(cfg.data.apolar.processed.path)
-    df_partial_apolar = get_data(cfg.data.apolar.final.path)
+    space_poli = [Real(1e-5, 100, "log-uniform", name="regressor__reg__alpha")]
 
-    print(df_partial_apolar.head())
+    space_poli_test = [
+        Real(
+            cfg.optimizing.poly_space.space.alpha.low,
+            cfg.optimizing.poly_space.space.alpha.high,
+            prior=cfg.optimizing.poly_space.space.alpha.prior,
+            name=cfg.optimizing.poly_space.space.alpha.name,
+        )
+    ]
+
+    print(space_poli == space_poli_test)
 
 
 if __name__ == "__main__":
