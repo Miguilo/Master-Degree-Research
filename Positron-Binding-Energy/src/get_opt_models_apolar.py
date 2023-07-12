8e8a8ae5fe58e8e8dcc9abc8eac7b4b4dc86a23a
Miguel Senra <mfasenra@hotmail.com>
2023-07-12 14:09:24 -0300

    fixed get_opt_models_apolar.py

--- Positron-Binding-Energy/src/get_opt_models_apolar.py
+++ Positron-Binding-Energy/src/get_opt_models_apolar.py
@@ -139,10 +139,10 @@ def main(cfg: DictConfig):
     initial_t = datetime.now()
     for i, j in enumerate(list_of_x_all):
         print(f"=== {list_of_features[i]} Features ===")
-        modify_scaling(list_of_models, list_of_models_names, j, list_of_features[i])
+        new_list_of_models = modify_scaling(list_of_models, list_of_models_names, list_of_features[i])
     
         opt_all(
-            list_of_models,
+            new_list_of_models,
             list_of_models_names,
             list_of_spaces,
             j,
@@ -204,9 +204,9 @@ def main(cfg: DictConfig):
 
     for i, j in enumerate(list_of_x_partial_iso):
         print(f"=== {list_of_features[i]} Features ===")
-        modify_scaling(list_of_models, list_of_models_names, j, list_of_features[i])
+        new_list_of_models = modify_scaling(list_of_models, list_of_models_names, list_of_features[i])
         opt_all(
-            list_of_models,
+            new_list_of_models,
             list_of_models_names,
             list_of_spaces,
             j,
@@ -220,9 +220,9 @@ def main(cfg: DictConfig):
 
     for i, j in enumerate(list_of_x_partial_aniso):
         print(f"=== {list_of_features[i]} Features ===")
-        modify_scaling(list_of_models, list_of_models_names, j, list_of_features[i])
+        new_list_of_models = modify_scaling(list_of_models, list_of_models_names, list_of_features[i])
         opt_all(
-            list_of_models,
+            new_list_of_models,
             list_of_models_names,
             list_of_spaces,
             j,
