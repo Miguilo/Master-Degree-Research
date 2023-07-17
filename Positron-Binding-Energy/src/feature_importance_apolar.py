f4a710a776eb1dc66e90bcacbc96dbb1c66edd8b
Miguel Senra <mfasenra@hotmail.com>
2023-07-17 14:31:57 -0300

    created feat importance for apolar molecules

---
+++ Positron-Binding-Energy/src/feature_importance_apolar.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append("../")
+
+import hydra
+from omegaconf import DictConfig
+from utils.data import get_absolute_path, get_data
+
+
+@hydra.main(config_path="../../config", config_name="main.yaml")
+def main(cfg: DictConfig):
+    path = cfg.data.polar.final.path
+    print(path)
+    return
+
+
+if __name__ == "__main__":
+    main()
