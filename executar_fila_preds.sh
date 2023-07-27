f4901bb479c6d9534228b12aea3dfa83ba1729e5
Miguel Senra <mfasenra@hotmail.com>
2023-07-27 13:26:40 -0300

    updated executar_fila_preds.sh

--- executar_fila_preds.sh
+++ executar_fila_preds.sh
@@ -3,6 +3,7 @@
 # Pasta de destino dos arquivos de saída
 pasta_saida="txt_outputs"
 
+# Verifica se a pasta de saída existe, caso contrário, cria-a
 if [ ! -d "$pasta_saida" ]; then
     mkdir "$pasta_saida"
 fi
@@ -14,16 +15,26 @@ arquivos=(
     "src_polar_apolar/get_test_preds_polar_apolar.py"
 )
 
-# Verifica se a pasta de saída existe, caso contrário, cria-a
-
+# Salva o diretório atual
+diretorio_atual=$(pwd)
 
 # Loop para processar cada arquivo da fila
 for arquivo in "${arquivos[@]}"; do
     nome_arquivo_saida="${arquivo%.*}_output.txt"  # Gera o nome do arquivo de saída
-    caminho_arquivo_saida="$pasta_saida/$nome_arquivo_saida"  # Caminho completo do arquivo de saída
+    caminho_arquivo_saida="$diretorio_atual/$pasta_saida/$nome_arquivo_saida"  # Caminho completo do arquivo de saída
     echo "Arquivo $arquivo adicionado à fila. Saída em $caminho_arquivo_saida"
-    nohup poetry run python3 "$arquivo" > "$caminho_arquivo_saida"
+    
+    # Altera o diretório atual para o diretório do arquivo
+    cd "$(dirname "$arquivo")"
+    
+    # Executa o script Python
+    nohup poetry run python3 "$(basename "$arquivo")" > "$caminho_arquivo_saida"
+    
+    # Volta ao diretório original
+    cd "$diretorio_atual"
+    
     wait  # Aguarda a conclusão da execução atual antes de prosseguir
 done
 
 echo "Todos os arquivos foram executados."
+
